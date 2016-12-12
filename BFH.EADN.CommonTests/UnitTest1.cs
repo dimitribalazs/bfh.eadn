6e297dd3f682da0326a7cffd2034f903ac412a26
Dimitri Balazs <dimitri.balazs@gmail.com>
2016-12-12 16:58:53 +0100

    added first projects to solution

---
+++ BFH.EADN.CommonTests/UnitTest1.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace BFH.EADN.CommonTests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
