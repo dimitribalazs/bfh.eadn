90e0b6df2f0b03ff842185b1e73ed8754709d06c
Dimitri Balazs <dimitri.balazs@gmail.com>
2016-12-12 21:17:38 +0100

    implemented method to read from config file. added test

--- BFH.EADN.CommonTests/UnitTest1.cs
+++ BFH.EADN.CommonTests/UnitTest1.cs
@@ -1,14 +1,28 @@
 ﻿using System;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Collections.Generic;
 
 namespace BFH.EADN.CommonTests
 {
     [TestClass]
-    public class UnitTest1
+    public class CommonTest
     {
         [TestMethod]
-        public void TestMethod1()
+        [ExpectedException(typeof(KeyNotFoundException))]
+        public void GetConfigValueFailedException()
         {
+            object result = Common.Common.GetConfigValue<int>(null);
+        }
+
+
+        [TestMethod]
+        public void GetConfigValueSuccess()
+        {
+            var intResult = Common.Common.GetConfigValue<int>("year");
+            var stringResult = Common.Common.GetConfigValue<string>("message");
+
+            Assert.AreEqual(1950, intResult);
+            Assert.AreEqual("this is a text", stringResult);
         }
     }
 }
