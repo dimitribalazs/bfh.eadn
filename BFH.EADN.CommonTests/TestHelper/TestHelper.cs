2e6de85b0518109cc67b7757bf87969e13e5ca4b
Dimitri Balazs <dimitribalazs@gmail.com>
2017-02-21 22:44:19 +0100

    generic property test

---
+++ BFH.EADN.CommonTests/TestHelper/TestHelper.cs
@@ -0,0 +1,116 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Ploeh.AutoFixture;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BFH.EADN.CommonTests.TestHelper
+{
+    public class TestHelper
+    {
+
+        /// <summary>
+        /// Checks all primitive types and some other types like Guid, string. 
+        /// If it's not a primitive type result is true
+        /// </summary>
+        /// <typeparam name="T">class to test</typeparam>
+        /// <returns></returns>
+        public static bool TestProperties<T>()
+            where T : class
+        {
+            Fixture fixture = new Fixture();
+            T instance = fixture.Build<T>().Create();
+            foreach (PropertyInfo property in instance.GetType().GetProperties())
+            {
+                bool testResult = false;
+
+                Type currentProperty = property.PropertyType;
+                if (currentProperty == typeof(bool)) 
+                {
+                    testResult = TestProperty<bool, T>(property, instance);
+                }
+                else if (currentProperty == typeof(Guid))
+                {
+                    testResult = TestProperty<Guid, T>(property, instance);
+                }
+                else if (currentProperty == typeof(byte))
+                {
+                    testResult = TestProperty<byte, T>(property, instance);
+                }
+                else if(currentProperty == typeof(sbyte))
+                {
+                    testResult = TestProperty<sbyte, T>(property, instance);
+                }
+                else if (currentProperty == typeof(char))
+                {
+                    testResult = TestProperty<char, T>(property, instance);
+                }
+                else if (currentProperty == typeof(decimal))
+                {
+                    testResult = TestProperty<decimal, T>(property, instance);
+                }
+                else if (currentProperty == typeof(double))
+                {
+                    testResult = TestProperty<double, T>(property, instance);
+                }
+                else if (currentProperty == typeof(float))
+                {
+                    testResult = TestProperty<float, T>(property, instance);
+                }
+                else if (currentProperty == typeof(int))
+                {
+                    testResult = TestProperty<int, T>(property, instance);
+                }
+                else if (currentProperty == typeof(uint))
+                {
+                    testResult = TestProperty<uint, T>(property, instance);
+                }
+                else if (currentProperty == typeof(long))
+                {
+                    testResult = TestProperty<long, T>(property, instance);
+                }
+                else if (currentProperty == typeof(ulong))
+                {
+                    testResult = TestProperty<ulong, T>(property, instance);
+                }
+                else if (currentProperty == typeof(short))
+                {
+                    testResult = TestProperty<short, T>(property, instance);
+                }
+                else if (currentProperty == typeof(ushort))
+                {
+                    testResult = TestProperty<ushort, T>(property, instance);
+                }
+                else if (currentProperty == typeof(string))
+                {
+                    testResult = TestProperty<string, T>(property, instance);
+                }
+                else if(currentProperty.IsPrimitive == false)
+                {
+                    testResult = true;
+                }
+
+                if(testResult == false)
+                {
+                    return false;
+                }
+            }
+
+            return true;
+        }
+
+        public static bool TestProperty<TPropertyType, TInstance>(PropertyInfo property, TInstance instance)
+            where TInstance : class
+        {
+            Fixture fixture = new Fixture();
+            TPropertyType input = fixture.Create<TPropertyType>();
+            property.SetValue(instance, input);
+            TPropertyType output = (TPropertyType)property.GetValue(instance);
+
+            return input.Equals(output);
+        }
+    }
+}
---
+++ BFH.EADN.CommonTests/Types.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Text;
+using System.Collections.Generic;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using BFH.EADN.Common.Types.Contracts;
+
+namespace BFH.EADN.CommonTests
+{
+    /// <summary>
+    /// Summary description for Types
+    /// </summary>
+    [TestClass]
+    public class Types
+    {
+        public Types()
+        {
+            //
+            // TODO: Add constructor logic here
+            //
+        }
+
+        private TestContext testContextInstance;
+
+        /// <summary>
+        ///Gets or sets the test context which provides
+        ///information about and functionality for the current test run.
+        ///</summary>
+        public TestContext TestContext
+        {
+            get
+            {
+                return testContextInstance;
+            }
+            set
+            {
+                testContextInstance = value;
+            }
+        }
+
+        #region Additional test attributes
+        //
+        // You can use the following additional attributes as you write your tests:
+        //
+        // Use ClassInitialize to run code before running the first test in the class
+        // [ClassInitialize()]
+        // public static void MyClassInitialize(TestContext testContext) { }
+        //
+        // Use ClassCleanup to run code after all tests in a class have run
+        // [ClassCleanup()]
+        // public static void MyClassCleanup() { }
+        //
+        // Use TestInitialize to run code before running each test 
+        // [TestInitialize()]
+        // public void MyTestInitialize() { }
+        //
+        // Use TestCleanup to run code after each test has run
+        // [TestCleanup()]
+        // public void MyTestCleanup() { }
+        //
+        #endregion
+
+        [TestMethod]
+        public void Contracts()
+        {
+            bool successAnswer = TestHelper.TestHelper.TestProperties<Answer>();
+            bool successQuestion = TestHelper.TestHelper.TestProperties<Question>();
+            bool successQuiz = TestHelper.TestHelper.TestProperties<Quiz>();
+            bool successServiceFault = TestHelper.TestHelper.TestProperties<ServiceFault>();
+            bool successTopic = TestHelper.TestHelper.TestProperties<Topic>();
+            bool successUser = TestHelper.TestHelper.TestProperties<User>();
+
+            Assert.IsTrue(successAnswer, "Answer property checks failed");
+            Assert.IsTrue(successQuestion, "Question property checks failed");
+            Assert.IsTrue(successQuiz, "Quiz property checks failed");
+            Assert.IsTrue(successServiceFault, "ServiceFault property checks failed");
+            Assert.IsTrue(successTopic, "Topic property checks failed");
+            Assert.IsTrue(successUser, "User property checks failed");
+        }
+    }
+}
